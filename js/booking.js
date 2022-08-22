5b8e88811481629b4fef8af5e0170cc01a7286ab
shivnath2k <shiv.nath@people10.com>
2022-08-22 20:04:52 +0530

    added form validation for booking page

---
+++ js/booking.js
@@ -0,0 +1,192 @@
+// var elem;
+// var firstname;
+// var lastname;
+
+// function loading()
+// {
+//     firstname=document.getElementById("first_name");
+//     lastname=document.getElementById("first_name");
+
+//     elem=document.getElementById("first_name");
+//     elem.addEventListener('input',fname);
+// }
+
+
+// function fname()
+// {
+//     console.log("Listener is running");
+//     var x=elem.value;
+//     if(x.length>2)
+//     {    elem.style.borderColor="red";
+//         console.log("Color changed to red");
+//         elem.style.borderSize="2em";
+//     }
+// }
+
+var name_regex = /^[a-z A-Z]+$/;
+var email_regex = /\S+@\S+\.com/;
+const items={
+    'first_name': 0, 'last_name':0, 'age':0, 'email':0, 'phone':0
+}
+
+function invalid(elm)
+{
+    elm.style.borderColor="red";
+    items[elm.id]=0;
+}
+function valid(elm)
+{
+    elm.style.borderColor="black";
+    items[elm.id]=1;
+    enablebutton();
+}
+
+function enablebutton()
+{
+    for (const item in items)
+    {
+        
+        if(item===0)
+        {
+            console.log("test")
+            return;
+        }
+    }
+    document.getElementById("submit").removeAttribute("disabled");
+}
+
+function v_firstname(elm)
+{
+    
+    var msg=document.getElementById("s_first_name");
+    if(elm.value.length==0)
+    {
+        invalid(elm);
+        msg.innerHTML="First name cannot be Empty";
+        return;
+    }
+    if(elm.value.length>20)
+    {
+        invalid(elm);
+        msg.innerHTML="First name cannot be more than 20 characters";
+        return;
+    }
+    if(name_regex.test(elm.value))
+    {
+        valid(elm);
+        msg.innerHTML="";
+    }
+    else
+    {
+        invalid(elm);
+        msg.innerHTML="First name can only be alphabets";
+    } 
+}
+
+
+function v_lastname(elm)
+{
+    
+    var msg=document.getElementById("s_last_name");
+    if(elm.value.length==0)
+    {
+        invalid(elm);
+        msg.innerHTML="Last name cannot be Empty";
+        return;
+    }
+    if(elm.value.length>20)
+    {
+        invalid(elm);
+        msg.innerHTML="Last name cannot be more than 20 characters";
+        return;
+    }
+    if(name_regex.test(elm.value))
+    {
+        valid(elm);
+        msg.innerHTML="";
+    }
+    else
+    {
+        invalid(elm);
+        msg.innerHTML="Last name can only be alphabets";
+    } 
+}
+
+
+function v_age(elm)
+{
+    var msg=document.getElementById("s_age");
+    if(elm.value.length==0)
+    {
+        invalid(elm);
+        innerHTML="Age cannot be empty";
+        return;
+    }
+    if(elm.value<18)
+    {
+        invalid(elm);
+        msg.innerHTML="Age cannot be less than 18";
+        return;
+    }
+    if(elm.value>999)
+    {
+        invalid(elm);
+        msg.innerHTML="Enter proper age";
+    }
+    else{
+        valid(elm);
+        msg.innerHTML="";
+    }
+}
+
+
+function v_email(elm)
+{
+    var msg=document.getElementById("s_email");
+    if(elm.value.length==0)
+    {
+        invalid(elm);
+        msg.innerHTML="Email cannot be Empty";
+        return;
+    }
+    if(email_regex.test(elm.value))
+    {
+        valid(elm);
+        msg.innerHTML="";
+    }
+    else
+    {
+        invalid(elm);
+        msg.innerHTML="Enter proper email";
+    } 
+}
+
+
+function v_phone(elm)
+{
+    var msg=document.getElementById("s_phone");
+    var phone_regex = /^[0-9]+$/;
+    if(elm.value.length==0)
+    {
+        invalid(elm);
+        msg.innerHTML="Phone Number cannot be Empty";
+        return;
+    }
+    if(elm.value.length>10)
+    {
+        invalid(elm);
+        msg.innerHTML="Phone number cannot be more than 10 digits";
+        return;
+    }
+    if(phone_regex.test(elm.value))
+    {
+        valid(elm);
+        msg.innerHTML="";
+    }
+    else
+    {
+        invalid(elm);
+        msg.innerHTML="Phone number can only be numbers";
+    }
+    
+}
